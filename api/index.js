9d29c38dca10c083c1aeeefcaad0aa0384ce572d
Sandipan Mahata <sandipan.mahata@bp.com>
2025-08-07 13:50:18 +0530

    done

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import app from './server.js';
+import serverLessExpress from "@vendia/serverless-express";
+import { connectMongo } from "./src/models/db.js";
+
+let serverlessExpressInstance
+
+async function setup (event, context) {
+ await connectMongo(config.db.mongoUrl);
+
+  serverlessExpressInstance = serverLessExpress({ app })
+  return serverlessExpressInstance(event, context)
+}
+
+function handlerFn (event, context) {
+  if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
+
+  return setup(event, context)
+}
+
+
+// async function start() {
+//   await connectMongo(config.db.mongoUrl);
+//   app.listen(config.server.port, () => {
+//     console.log(`Server running on port ${config.server.port}`);
+//   });
+// }
+
+// start();
+
+export const handler = handlerFn;
